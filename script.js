692bdae33d13da3939668183423b09fe09134c88
Malick Roungou <mjbroungou@gmail.com>
2024-07-13 09:41:58 -0400

    updates

--- script.js
+++ script.js
@@ -5,16 +5,110 @@ const mortgageAmt = document.getElementById('mortgage-amt');
 const interestRate = document.getElementById('interest-rate');
 const repaymentOption = document.getElementById('repayment');
 const interestOption = document.getElementById('interest');
+const monthlyPmtSpan = document.getElementById('monthly-pmt');
+const totalPmtSpan = document.getElementById('total-pmt');
+const emptyResultsDiv = document.getElementById('empty-results');
+const completedResultsDiv = document.getElementById('completed-results');
+
+const inputs = document.querySelectorAll('input[type="text"]');
+const radios = document.querySelectorAll('input[type="radio"]');
 
 function calculate() {
-    const amt = mortgageAmt.value;
-    const interest = interestRate.value / 100;
-    const term = yearsInput.value;
+    const amt = parseFloat(mortgageAmt.value);
+    const interest = parseFloat(interestRate.value) / 100;
+    const term = parseInt(yearsInput.value);
 
     if(interestOption.checked) {
         const monthlyPmt = (interest * amt) / 12;
-        console.log(monthlyPmt);
+        monthlyPmtSpan.innerText = `${new Intl.NumberFormat('en-GB', {style: 'currency', currency: 'GBP'}).format(monthlyPmt)}`;
+        totalPmtSpan.innerText = `${new Intl.NumberFormat('en-GB', {style: 'currency', currency: 'GBP'}).format(monthlyPmt * 12 * term)}`;
+    } else if(repaymentOption.checked) {
+        const r = interest / 12 // gives you the monthly interest rate
+        const n = term * 12 // gives you the number of payments to be made
+        const monthlyPmt = amt * (r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
+        monthlyPmtSpan.innerText = `${new Intl.NumberFormat('en-GB', {style: 'currency', currency: 'GBP'}).format(monthlyPmt)}`;
+        totalPmtSpan.innerText = `${new Intl.NumberFormat('en-GB', {style: 'currency', currency: 'GBP'}).format(monthlyPmt * n)}`;
     }
 }
 
-calculateBtn.addEventListener('click', calculate)
+function hasError(element) {
+    const parentDiv = element.parentElement;
+    if (parentDiv) {
+        parentDiv.classList.add('has-error')
+        if (!parentDiv.querySelector('.error-message')){
+            const errorDiv = document.createElement('div');
+            errorDiv.className = 'error-message';
+            errorDiv.textContent = 'This field is required';
+            parentDiv.appendChild(errorDiv);
+            // parentDiv.innerHTML += `<div class = "error-message">This field is required</div>`;
+        }
+    }
+
+}
+
+function hasNoError(element) {
+    const parentDiv = element.parentElement;
+    if (parentDiv) {
+        parentDiv.classList.remove('has-error');
+        const errorMessage = parentDiv.querySelector('.error-message');
+        if (errorMessage) {
+            errorMessage.remove()
+        }
+    }
+}
+
+clearBtn.addEventListener('click', () => {
+    inputs.forEach(input => {
+        input.value = '';
+        hasNoError(input);
+    })
+
+    radios.forEach(radio => {
+        radio.checked = false;
+        hasNoError(radio.parentElement)
+    })
+
+    emptyResultsDiv.style.display = 'block';
+    completedResultsDiv.style.display = 'none';
+    monthlyPmtSpan.innerText = '';
+    totalPmtSpan.innerText = '';
+} )
+
+
+function validateRadios(radios) {
+    let isChecked = false;
+    radios.forEach(radio => {
+        if (radio.checked) {
+            isChecked = true;
+        }
+    });
+
+    if (!isChecked) {
+        hasError(radios[0].parentElement); // Assuming all radios have the same parent
+        return false;
+    } else {
+        hasNoError(radios[0].parentElement); // Assuming all radios have the same parent
+        return true;
+    }
+}
+
+calculateBtn.addEventListener('click', () => {
+    let allInputFields = true;
+
+    inputs.forEach(input => {
+        if (input.value === '') {
+            hasError(input)
+            allInputFields = false;
+        } else {
+            hasNoError(input)
+        }
+    });
+
+    const radiosValid = validateRadios(radios);
+    
+    if (allInputFields && radiosValid) {
+        calculate();
+        emptyResultsDiv.style.display = 'none';
+        completedResultsDiv.style.display = 'block';
+    }
+});
